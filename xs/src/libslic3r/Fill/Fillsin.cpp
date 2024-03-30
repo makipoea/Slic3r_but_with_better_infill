5d028548acf8d4d73b1711f8dc12da5ebd7ba3db
makipoea <makipoea@gmail.com>
2024-03-30 17:38:19 +0100

    adding cube.stl/compile script/ Fillsin

---
+++ xs/src/libslic3r/Fill/Fillsin.cpp
@@ -0,0 +1,10 @@
+#include "../ClipperUtils.hpp" //configuration of clipper (and basic function like offcet)
+
+#include "../PolylineCollection.hpp" //defintion of polyline object like struct Chaining 
+#include "../Surface.hpp"  // No idea
+
+#include "Fillsin.hpp"
+
+namespace Slic3r {
+
+}
